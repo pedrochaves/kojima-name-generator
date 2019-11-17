7701558a3091a4b326f984e0a4dc772f7b3e736c
Pedro Chaves <pcohc3m@gmail.com>
2019-11-17 13:32:07 -0300

    Updates

---
+++ precache-manifest.5fae01bef5494f2642f0f64fbabc5170.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ccef9239fb95a5d8e2ec60221c4ae1",
+    "url": "/kojima-name-generator/index.html"
+  },
+  {
+    "revision": "d305bc2ca6335f3a4ffd",
+    "url": "/kojima-name-generator/static/css/main.f0f8fcc7.chunk.css"
+  },
+  {
+    "revision": "f75c1fb94e51aed8990f",
+    "url": "/kojima-name-generator/static/js/2.6d1f6041.chunk.js"
+  },
+  {
+    "revision": "d305bc2ca6335f3a4ffd",
+    "url": "/kojima-name-generator/static/js/main.9693c88c.chunk.js"
+  },
+  {
+    "revision": "fc5416d7eff6d9e3e438",
+    "url": "/kojima-name-generator/static/js/runtime-main.f9f28000.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/kojima-name-generator/precache-manifest.5fae01bef5494f2642f0f64fbabc5170.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kojima-name-generator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
