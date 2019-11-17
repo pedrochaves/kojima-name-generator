f3ed26711dfb66a3a11e5f82d1775ba23dbef3ea
Pedro Chaves <pcohc3m@gmail.com>
2019-11-17 15:58:46 -0300

    Adds warning about project

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Kojima Name Generator
 
 This is a small app based on the formula created by Brian David Gilbert from Polygon,
-which allows us to create our Hideo Kojima name. You can see the formula in [https://www.polygon.com/videos/2019/11/11/20959269/unraveled-kojima-name-generator-death-stranding](this article) or watch the video on YouTube.
+which allows us to create our Hideo Kojima name. You can see the formula in [this article](https://www.polygon.com/videos/2019/11/11/20959269/unraveled-kojima-name-generator-death-stranding) or watch the [original video on YouTube](https://www.youtube.com/watch?v=t-3i6GBYvdw&feature=emb_title).
+
+The app is available on https://pedrochaves.github.io/kojima-name-generator
+
+
+## About Ownership
+
+This project is not associated with Brian David Gilbert or Polygon by any means. It was created in my spare time just for fun.
+I have no intention of using this to make money or steal the awesome and fun formula created by BDG.
+
+## About Personal Data
+
+Absolutely no personal data is being collected in this small application, I guarantee.
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {
-  Jumbotron,
-  Button,
-  Row,
-  Col,
+    Alert,
+    Jumbotron,
+    Button,
+    Row,
+    Col,
 } from 'react-bootstrap';
 
 const Intro = (props) => {
@@ -14,8 +15,22 @@ const Intro = (props) => {
                     <h1>Kojima Name Generator</h1>
                     <p>
                     This is a small app based on the formula created by Brian David Gilbert from Polygon,
-                    which allows us to create our Hideo Kojima name. You can see the formula in <a href="https://www.polygon.com/videos/2019/11/11/20959269/unraveled-kojima-name-generator-death-stranding" target="_blank" rel="noopener noreferrer">this article</a> or watch the video ->
+                    which allows us to create our Hideo Kojima name.
                     </p>
+                    <p>
+                    You can see the formula in <a href="https://www.polygon.com/videos/2019/11/11/20959269/unraveled-kojima-name-generator-death-stranding" target="_blank" rel="noopener noreferrer">this article</a> or watch this video right here.
+                    </p>
+                    <Alert variant="secondary">
+                        <p>
+                            This project is not associated with Polygon or Brian David Gilbert by any means. It was made
+                            for fun during the creator's spare time.
+                        </p>
+                        <hr/>
+                        <p>
+                            Absolutely <strong>no personal data</strong> is being collected from you, so you can play around
+                            all you want. If you are still suspicious, you can check out the code on <a href="https://github.com/pedrochaves/kojima-name-generator">GitHub</a> (and collaborate if you want).
+                        </p>
+                    </Alert>
                 </Col>
                 <Col>
                     <p>
